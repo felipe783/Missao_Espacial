a2cf1a3863b3600b2612daa5bf9f34ba1989aef7
unknown <felipebolt2007@gmail.com>
2026-03-19 20:32:56 -0300

    Nova COnnection

--- src/main/java/org/example/Data/ConnectionData.java
+++ src/main/java/org/example/Data/ConnectionData.java
@@ -1,27 +1,26 @@
 package org.example.Data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.Connection; //Criar conexão com SQL
-import java.sql.DriverManager; //Conexçao JDBC
 
-public class ConnectionData {
-
-    private static Connection connection; //Cria a variavel do tipo Connection
-
-    public static Connection getConnection() throws Exception{
+public class ConnectionData{
+    private static Connection connection;
+    public static synchronized Connection getConnection() throws SQLException {
         /*
         Aqui basicamente ele cria a conexão ce não existir ou reutiliza
         o GetConnection() pega a conexão com o banco de dados
         "throws Exception" avisa pro Java que o metodo pode dar erro e ele meio q ignora os erros
         */
-        if (connection == null || connection.isClosed()) { //Verifica ce conectou ou se a conexão foi fechada
-            //Carrega a ENV e puxa as variaveis
+        if (connection == null || connection.isClosed()) { //Verifica se a conexão nunca foi criada ou foi fechada
             Dotenv dotenv = Dotenv.load();
+
             String url = dotenv.get("DB_URL");
             String user = dotenv.get("DB_USER");
             String password = dotenv.get("DB_PASSWORD");
 
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password); //Driver manager Procura o drive do banco(Mysql)
             /*
             Não da recursão na função por conta dela Chamar o DriverManager.getConnection(...) esse get é do drivermanager
             DriverManager é onde gerencia os drivers do DB
@@ -30,5 +29,11 @@ public class ConnectionData {
         }
 
         return connection;
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) { //So fecha ce a conexão existe e ta aberta
+            connection.close();
+        }
     }
 }
