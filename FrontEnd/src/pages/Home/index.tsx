59906be4a5ae5b352fccbbcd9b6373816ae359ae
unknown <felipebolt2007@gmail.com>
2026-05-08 06:42:14 -0300

    Começo front

---
+++ FrontEnd/src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import './sytle.css'
+
+function Home() {
+    return (
+        <>
+            <div className="container-options">
+                <h1>Astronautas</h1>
+                <h1>Equipes</h1>
+                <h1>Foguetes</h1>
+                <h1>Missão</h1>
+            </div>
+
+            <div className="main-title">
+                <span id="title-mission">Missão</span>
+                <span id="title-special">Espacial</span>
+            </div>
+
+            <div className="logo">
+            </div>
+        </>
+    )
+
+}
+
+export default Home
